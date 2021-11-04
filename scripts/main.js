6ca8ee50689e0edc46cc99527718392d588b2d9d
dorfingerjonas <jonas.dorfinger@gmx.at>
2021-11-04 22:46:33 +0100

    add main.js

---
+++ scripts/main.js
@@ -0,0 +1,8 @@
+window.addEventListener('load', () => {
+    new Typed('#slogan', {
+        strings: ['On Demand Software Development'],
+        typeSpeed: 50,
+        backSpeed: 50,
+        showCursor: false
+    });
+});
